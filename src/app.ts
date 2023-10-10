773405b4ca90d23bf5c09af77e40c7120b5c4326
rafael-horauti <rafael.horauti@uiclap.com>
2023-10-10 15:46:05 -0300

    inclusão de tipagem no retorno da função e reaproveitamento de código

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,33 @@
 // 1 - Criar uma função que retorne a quantidade de vogais da palavra passada. Teste1
 const vogais: Array<string> = ['a', 'e', 'i', 'o', 'u'];
 
-function retornarQtdVogais(palavra: string) {
-    let qtdVogais = 0
-    const arrayPalavra = palavra.split('');
-    console.log(arrayPalavra)
+const retornoQtdVogais = function retornarQtdVogais(palavra: string) : number | string {
+    let qtdVogais: number = 0
+    const arrayPalavra: Array<string> = palavra?.split('');
     if(arrayPalavra.length == 0) {
-        return console.log('Nenhuma palavra foi digitada!');
+        return 'Nenhuma palavra foi digitada!';
     } else {
         vogais.forEach((v: string) => {
             arrayPalavra.forEach((p: string) => {
                 if(p.normalize("NFD").replace(/\p{Diacritic}/gu, "") == v) qtdVogais++
             }) 
         })
-        return console.log(qtdVogais)
+        return qtdVogais
     }
 }
 
 // a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
-retornarQtdVogais('bootcamp');
+retornoQtdVogais('bootcamp');
 //b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
 
 function mostrarQtdVogais() {
-    let qtdVogais = 0
     let qtdVogaisPalavra: HTMLElement = document.getElementById('qtd-vogais-palavra');
     const arrayPalavra: Array<string> = document.getElementById('input-palavra')?.value.split('');
     if(arrayPalavra.length == 0 || arrayPalavra == null || arrayPalavra == undefined) {
         alert('Nenhuma palavra foi digitada!');
         qtdVogaisPalavra.innerHTML = '';
     } else {
-        vogais.forEach((vogal: string) => {
-            arrayPalavra.forEach((letra: string) => {
-                if(letra.normalize("NFD").replace(/\p{Diacritic}/gu, "") == vogal) qtdVogais++;
-            }) 
-        })
-        qtdVogaisPalavra.innerHTML = `A quantidade de vogais da plavara digitada é <b>${qtdVogais}</b>`; 
+        qtdVogaisPalavra.innerHTML = `A quantidade de vogais da plavara digitada é <b>${retornoQtdVogais}</b>`; 
     }
 }
 
